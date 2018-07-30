3ea225fcfa17bb737a02664c8759c6394e94206e
Jason Arnold <jason.arnold.00@gmail.com>
2018-07-30 15:32:23 -0400

    book component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -2,32 +2,20 @@ import React, { Component } from 'react'
 import Book from './Book'
 
 class Bookshelf extends Component {
-  state = {
-    books: [
-     {
-       backgroundImage: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-       title: "To Kill a Mockingbird",
-       author: "Harper Lee"
-     },
-     {
-       backgroundImage: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-       title: "Ender's Game",
-       author: "Orson Scott Card"
-     }
-    ]
-  }
   render() {
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.props.title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {this.state.books.forEach(book => (
-              <Book
-                backgroundImage={book.backgroundImage}
-                title={book.title}
-                author={book.author}
-              />
+            {this.props.books.map(book => (
+              <li key={book.title}>
+                <Book
+                  backgroundImage={book.backgroundImage}
+                  title={book.title}
+                  author={book.author}
+                />
+              </li>
             ))}
           </ol>
         </div>
