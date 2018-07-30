fabf5de74f9148e23a8f89102d82a94e71eef5b6
Jason Arnold <jason.arnold.00@gmail.com>
2018-07-30 15:11:21 -0400

    tyring to build out book component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchList from './SearchList'
